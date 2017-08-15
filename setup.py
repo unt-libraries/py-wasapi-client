8f210e548a3a9f2984e323a67660fdee8dfe7823
Lauren Ko <lauren.ko@unt.edu>
2017-08-15 11:52:34 -0500

    Currently runs in Python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -25,10 +25,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Communications :: File Sharing',
     ],
 )
